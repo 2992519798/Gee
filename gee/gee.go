8949f7a88543d50e132a3b29746228dece0a3e5b
yt <yintong@ustc.edu>
2022-11-16 16:39:58 +0800

    gee overall framework

--- gee/gee.go
+++ gee/gee.go
@@ -1,8 +1,12 @@
 package gee
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // HandlerFunc defines the handler used by gee
+// 提供给框架用户，用来定义路由映射的处理方法
 type HandlerFunc func(w http.ResponseWriter, r *http.Request)
 
 type Engine struct {
@@ -13,11 +17,32 @@ func New() *Engine {
 	return &Engine{router: make(map[string]HandlerFunc)}
 }
 
-func (engin *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
+func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
 	key := method + "-" + pattern
-	engin.router[key] = handler
+	engine.router[key] = handler
 }
 
+// GET defines the method to add GET request
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRouter("GET", pattern, handler)
+}
 
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRouter("POST", pattern, handler)
+}
+
+// RUN  warps the method to start a http server
+func (engine *Engine) RUN(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// ServeHTTP implements the interface of a  Handler (Engine)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module gee
+module example
 
-go 1.13
+go 1.19
 
 require gee v0.0.0
 
